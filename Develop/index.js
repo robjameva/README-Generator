12e0e0092aecac6f739b2cfa0405eb40154cb002
Robert Evanik <rob.evanik@gmail.com>
2021-12-17 17:08:01 -0500

    inquirer set up

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,24 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)'
+        }
+    ]);
+};
+
 
 // Function call to initialize app
 init();
